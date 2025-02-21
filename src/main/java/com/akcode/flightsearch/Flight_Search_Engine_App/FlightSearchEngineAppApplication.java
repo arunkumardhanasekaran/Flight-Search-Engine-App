976bbb7c52812ada2fc0fe74be890c0c35090bd5
arunkumardhanasekaran <arunkumar1826@gmail.com>
2025-02-21 16:10:31 +0530

    Initial Commit

---
+++ src/main/java/com/akcode/flightsearch/Flight_Search_Engine_App/FlightSearchEngineAppApplication.java
@@ -0,0 +1,13 @@
+package com.akcode.flightsearch.Flight_Search_Engine_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchEngineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchEngineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akcode/flightsearch/Flight_Search_Engine_App/FlightSearchEngineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akcode.flightsearch.Flight_Search_Engine_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchEngineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
